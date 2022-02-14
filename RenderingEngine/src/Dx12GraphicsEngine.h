5ca3c40d2acdfa216bccbeead437832b65f33b1e
SSK-0423 <b1900446@planet.kanazawa-it.ac.jp>
2022-02-14 17:12:39 +0900

    #3-1 デバッグレイヤー有効～スワップチェーン生成

--- RenderingEngine/src/Dx12GraphicsEngine.h
+++ RenderingEngine/src/Dx12GraphicsEngine.h
@@ -1,11 +1,20 @@
 #pragma once
 #include "EngineUtility.h"
+#include "AppWindow.h"
+
+#include <d3d12.h>
+#include <dxgi1_6.h>
+
+#pragma comment(lib,"d3d12.lib")
+#pragma comment(lib,"dxgi.lib")
+
+#include <wrl.h>
 
 class Dx12GraphicsEngine
 {
 private:
 	Dx12GraphicsEngine();
-	~Dx12GraphicsEngine();
+	~Dx12GraphicsEngine() = default;
 	Dx12GraphicsEngine(const Dx12GraphicsEngine& inst) = delete;
 	void operator=(const Dx12GraphicsEngine& inst) = delete;
 
@@ -17,12 +26,63 @@ public:
 	static Dx12GraphicsEngine& Instance();
 
 	/// <summary>
-	/// DirectX12の初期化
+	/// DirectXで描画を行うための初期化処理
 	/// </summary>
+	/// <param name="hwnd">ウィンドウハンドル</param>
+	/// <param name="windowWidth">ウィンドウ幅</param>
+	/// <param name="windowHeight">ウィンドウ高</param>
+	/// <param name="dxgiFactory">ファクトリー</param>
 	/// <returns>MYRESULT::SUCCESS: 成功 MYRESULT::FAILED: 失敗</returns>
-	MYRESULT Init();
+	MYRESULT Init(const HWND& hwnd, const UINT& windowWidth, const UINT& windowHeight);
+private:
+	// DXGI関連
+	Microsoft::WRL::ComPtr<IDXGIFactory6> _dxgiFactory = nullptr;
+	Microsoft::WRL::ComPtr<IDXGISwapChain4> _swapchain = nullptr;
+
+	// DirectX12初期化関連
+	Microsoft::WRL::ComPtr<ID3D12Device> _device = nullptr;
+	Microsoft::WRL::ComPtr<ID3D12CommandAllocator> _cmdAllocator = nullptr;
+	Microsoft::WRL::ComPtr<ID3D12GraphicsCommandList> _cmdList = nullptr;
+	Microsoft::WRL::ComPtr<ID3D12CommandQueue> _cmdQueue = nullptr;
+
+	// ウィンドウ関連
+	HWND _hwnd;
+	UINT _windowWidth;
+	UINT _windowHeight;
+
+	/// <summary>
+	/// デバッグレイヤーを有効にする
+	/// </summary>
+	/// <returns></returns>
+	HRESULT EnableDebugLayer();
+	/// <summary>
+	/// デバイスとファクトリー生成
+	/// </summary>
+	/// <returns></returns>
+	HRESULT CreateDeviceAndDXGIFactory();
+	/// <summary>
+	/// コマンドアロケータ、リスト、キュー生成
+	/// </summary>
+	/// <returns></returns>
+	HRESULT CreateCommandX();
+	
+	/// <summary>
+	/// スワップチェーン生成
+	/// </summary>
+	/// <param name="hwnd">ウィンドウハンドル</param>
+	/// <param name="windowWidth">ウィンドウ幅</param>
+	/// <param name="windowHeightconst">ウィンドウ高</param>
+	/// <param name="dxgiFactory">ファクトリー</param>
+	/// <returns></returns>
+	HRESULT CreateSwapChain(
+		const HWND& hwnd, const UINT& windowWidth, const UINT& windowHeightconst,
+		const Microsoft::WRL::ComPtr<IDXGIFactory6>& dxgiFactory);
+
+public:
+
 };
 
 /// メモ
 /// MiniEngineはグローバル変数でポインタを持っていたのでよろしくない
 /// 同様のことをするならシングルトンの方が安全→デストラクタがprivateになっているのでどこかでdeleteされる心配がない
+/// メンバ関数の引数にメンバ変数を指定する→関数の処理内容、意図が明確に伝わる、影響範囲が明確になる
--- RenderingEngine/src/EngineUtility.h
+++ RenderingEngine/src/EngineUtility.h
@@ -1,4 +1,7 @@
 #pragma once
+#include <Windows.h>
+#include <wrl.h>
+
 /// <summary>
 /// 関数の成功/失敗
 /// </summary>
@@ -20,3 +23,27 @@ inline bool IsResultSuccess(MYRESULT result) { return result == MYRESULT::SUCCES
 /// <param name="result">関数の実行結果</param>
 /// <returns>true: MYRESULT::FAILED false: MYRESULT::SUCCESS</returns>
 inline bool IsResultFailed(MYRESULT result) { return result == MYRESULT::FAILED; }
+
+/// <summary>
+/// HRESULT型の引数が失敗を表しているか調べる
+/// </summary>
+/// <param name="result">関数の実行結果</param>
+/// <returns>true: 失敗 false: S_OK</returns>
+inline bool IsHresultFailed(HRESULT result) { return FAILED(result); }
+
+/// <summary>
+/// 関数の実行が失敗した場合に失敗を表す値を返し、関数の実行を中断する
+/// </summary>
+/// <param name="result">関数の実行結果</param>
+/// <returns></returns>
+inline MYRESULT ReturnFailed(HRESULT result) { if (FAILED(result)) return MYRESULT::FAILED; }
+
+/// <summary>
+/// 
+/// </summary>
+/// <typeparam name="T"></typeparam>
+/// <param name="obj"></param>
+template <class T>
+inline void ReleaseComObj(Microsoft::WRL::ComPtr<T> obj) {
+	if (obj.Get() != nullptr) {}
+}
