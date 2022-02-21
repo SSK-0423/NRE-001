dab60e8a403301edd861f08b5bb40e9c853ff500
SSK-0423 <b1900446@planet.kanazawa-it.ac.jp>
2022-02-21 19:08:38 +0900

    #9-3 ルートシグネチャクラス サンプラーの複数指定に対応

--- RenderingEngine/src/GraphicsPipelineState.h
+++ RenderingEngine/src/GraphicsPipelineState.h
@@ -11,7 +11,26 @@ class GraphicsPipelineState {
 public:
 	GraphicsPipelineState() = default;
 	~GraphicsPipelineState() = default;
+
 private:
-	Microsoft::WRL::ComPtr<ID3D12PipelineState> _pipelineState = nullptr;
+	Microsoft::WRL::ComPtr<ID3D12PipelineState> _pipelineState = nullptr;	// パイプラインステート
+
+	/// <summary>
+	/// パイプラインステート生成
+	/// </summary>
+	/// <param name="device">デバイス</param>
+	/// <param name="piplineStateDesc">グラフィックスパイプラインステート構造体</param>
+	/// <returns></returns>
+	HRESULT CreateGraphicsPipelineState(
+		ID3D12Device& device, const D3D12_GRAPHICS_PIPELINE_STATE_DESC& piplineStateDesc);
+
 public:
+	/// <summary>
+	/// パイプラインステート生成
+	/// </summary>
+	/// <param name="device">デバイス</param>
+	/// <param name="piplineStateDesc">グラフィックスパイプラインステート構造体</param>
+	/// <returns></returns>
+	MYRESULT Create(ID3D12Device& device, const D3D12_GRAPHICS_PIPELINE_STATE_DESC& piplineStateDesc);
+
 };
