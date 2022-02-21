d21c4da0eaf28430b5c1196d9eb340598a741d6a
SSK-0423 <b1900446@planet.kanazawa-it.ac.jp>
2022-02-21 19:08:38 +0900

    #9-3 ルートシグネチャクラス サンプラーの複数指定に対応

--- RenderingEngine/src/RootSignature.cpp
+++ RenderingEngine/src/RootSignature.cpp
@@ -3,38 +3,63 @@
 
 #include "RootSignature.h"
 
-RootSignatureData::RootSignatureData(
-	D3D12_TEXTURE_ADDRESS_MODE addressU, D3D12_TEXTURE_ADDRESS_MODE addressV,
-	D3D12_TEXTURE_ADDRESS_MODE addressW,
-	UINT srvDescriptorNum, UINT cbvDescriptorNum, UINT uavDescriptorNum)
-	: _addressU(addressU), _addressV(addressV), _addressW(addressW),
-	_srvDescriptorNum(srvDescriptorNum), _cbvDescriptorNum(cbvDescriptorNum),
+SamplerData::SamplerData(
+	D3D12_FILTER samplerFilter,
+	D3D12_TEXTURE_ADDRESS_MODE addressU,
+	D3D12_TEXTURE_ADDRESS_MODE addressV,
+	D3D12_TEXTURE_ADDRESS_MODE addressW)
+	: _samplerFilter(samplerFilter), _addressU(addressU), _addressV(addressV), _addressW(addressW)
+{}
+
+DescriptorRangeData::DescriptorRangeData(UINT srvDescriptorNum, UINT cbvDescriptorNum, UINT uavDescriptorNum)
+	: _srvDescriptorNum(srvDescriptorNum), _cbvDescriptorNum(cbvDescriptorNum),
 	_uavDescriptorNum(uavDescriptorNum)
 {}
 
-HRESULT RootSignature::CreateRootSignature(ID3D12Device& device, const RootSignatureData& data)
+RootSignatureData::RootSignatureData(
+	D3D12_FILTER samplerFilter,
+	D3D12_TEXTURE_ADDRESS_MODE addressU, D3D12_TEXTURE_ADDRESS_MODE addressV,
+	D3D12_TEXTURE_ADDRESS_MODE addressW,
+	UINT srvDescriptorNum, UINT cbvDescriptorNum, UINT uavDescriptorNum)
 {
-	CD3DX12_DESCRIPTOR_RANGE descTblRange[DESCRIPTORTYPE_NUM] = {};
-	descTblRange[DESCRIPTORTYPE_SRV].Init(D3D12_DESCRIPTOR_RANGE_TYPE_SRV, data._srvDescriptorNum, 0);
-	descTblRange[DESCRIPTORTYPE_CBV].Init(D3D12_DESCRIPTOR_RANGE_TYPE_CBV, data._cbvDescriptorNum, 0);
-	descTblRange[DESCRIPTORTYPE_UAV].Init(D3D12_DESCRIPTOR_RANGE_TYPE_UAV, data._uavDescriptorNum, 0);
+	_samplerData._samplerFilter = samplerFilter;
+	_samplerData._addressU = addressU;
+	_samplerData._addressV = addressV;
+	_samplerData._addressW = addressW;
 
+	_descRangeData._srvDescriptorNum = srvDescriptorNum;
+	_descRangeData._cbvDescriptorNum = cbvDescriptorNum;
+	_descRangeData._uavDescriptorNum = uavDescriptorNum;
+}
+
+RootSignatureData::RootSignatureData(SamplerData samplerData, DescriptorRangeData descRangeData)
+	: _samplerData(samplerData), _descRangeData(descRangeData)
+{}
+
+HRESULT RootSignature::CreateRootSignature(
+	ID3D12Device& device, const DescriptorRangeData& descRangeData,
+	const D3D12_STATIC_SAMPLER_DESC& samplerDescs, UINT samplerNum)
+{
+	// ディスクリプタレンジ
+	CD3DX12_DESCRIPTOR_RANGE descTblRange[DESCRIPTORTYPE_NUM] = {};
+	descTblRange[DESCRIPTORTYPE_SRV].Init(D3D12_DESCRIPTOR_RANGE_TYPE_SRV, descRangeData._srvDescriptorNum, 0);
+	descTblRange[DESCRIPTORTYPE_CBV].Init(D3D12_DESCRIPTOR_RANGE_TYPE_CBV, descRangeData._cbvDescriptorNum, 0);
+	descTblRange[DESCRIPTORTYPE_UAV].Init(D3D12_DESCRIPTOR_RANGE_TYPE_UAV, descRangeData._uavDescriptorNum, 0);
+
+	// ルートパラメータ
 	CD3DX12_ROOT_PARAMETER	rootPrams[DESCRIPTORTYPE_NUM] = {};
 	rootPrams[DESCRIPTORTYPE_SRV].InitAsDescriptorTable(1, &descTblRange[DESCRIPTORTYPE_SRV]);
 	rootPrams[DESCRIPTORTYPE_CBV].InitAsDescriptorTable(1, &descTblRange[DESCRIPTORTYPE_CBV]);
 	rootPrams[DESCRIPTORTYPE_UAV].InitAsDescriptorTable(1, &descTblRange[DESCRIPTORTYPE_UAV]);
 
-	// TODO:サンプラーは様々な設定に対応できるようにする
-	CD3DX12_STATIC_SAMPLER_DESC samplerDesc[1] = {};
-	samplerDesc[0].Init(0, D3D12_FILTER_ANISOTROPIC, data._addressU, data._addressV, data._addressW);
-
+	// ルートシグネチャ
 	CD3DX12_ROOT_SIGNATURE_DESC rootSignatureDesc = {};
 	rootSignatureDesc.Init(
-		_countof(rootPrams), rootPrams, _countof(samplerDesc), samplerDesc,
+		_countof(rootPrams), rootPrams, samplerNum, &samplerDescs,
 		D3D12_ROOT_SIGNATURE_FLAG_ALLOW_INPUT_ASSEMBLER_INPUT_LAYOUT);
 
-	Microsoft::WRL::ComPtr<ID3DBlob> rootSignatureBlob = nullptr;
-	Microsoft::WRL::ComPtr<ID3DBlob> errorBlob = nullptr;
+	Microsoft::WRL::ComPtr<ID3DBlob> rootSignatureBlob = nullptr;	// ルートシグネチャのバイナリ
+	Microsoft::WRL::ComPtr<ID3DBlob> errorBlob = nullptr;			// 
 
 	// ルートシグネチャのバイナリコード生成
 	HRESULT result = D3D12SerializeRootSignature(
@@ -52,8 +77,25 @@ HRESULT RootSignature::CreateRootSignature(ID3D12Device& device, const RootSigna
 
 MYRESULT RootSignature::Create(ID3D12Device& device, const RootSignatureData& data)
 {
-	if (FAILED(CreateRootSignature(device, data))) { return MYRESULT::FAILED; }
+	// STATIC_SAMPLER_DESC作成
+	SamplerData samplerData = data._samplerData;
+
+	CD3DX12_STATIC_SAMPLER_DESC samplerDesc = {};
+	samplerDesc.Init(
+		0, samplerData._samplerFilter, samplerData._addressU, samplerData._addressV, samplerData._addressW);
+
+	// ルートシグネチャ生成
+	return Create(device, data._descRangeData, samplerDesc, 1);
+}
+
+MYRESULT RootSignature::Create(
+	ID3D12Device& device, const DescriptorRangeData& descRangeData,
+	const D3D12_STATIC_SAMPLER_DESC& samplerDescs, UINT samplerNum)
+{
+	// ルートシグネチャ生成
+	if (FAILED(CreateRootSignature(device, descRangeData, samplerDescs, samplerNum))) {
+		return MYRESULT::FAILED;
+	}
 
 	return MYRESULT::SUCCESS;
 }
-
